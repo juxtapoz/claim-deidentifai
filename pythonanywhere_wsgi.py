03ca41807be9cadb975dc690e0260d4168a6ba9a
Scott Loveland <scott.loveland@gmail.com>
2025-01-15 15:57:54 -0800

    replace hypens

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-# Add the project root and ai-services to the Python path
+# Add the project root and ai_services to the Python path
 project_home = '/home/sloveland/claim-deidentifai'
-ai_services_path = os.path.join(project_home, 'ai-services')
+ai_services_path = os.path.join(project_home, 'ai_services')
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 if ai_services_path not in sys.path:
